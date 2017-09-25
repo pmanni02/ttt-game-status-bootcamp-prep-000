d8edf9a6c0dfcb0b20609d8aeca4b1ba83d1f14e
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-25 19:28:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |comb|
      (comb[0]===comb[1]&&comb[1]===comb[2]&&comb[0]===comb[2]) && (comb[0]==="X" || comb[0]==="O")
-      #winning_comb = comb
+      winning_comb = comb
   end
 
   if(winning_comb)
